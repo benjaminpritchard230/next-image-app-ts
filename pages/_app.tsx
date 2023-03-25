17d1c1332e1e81b676e19c5a90af2c62dca14b65
benjaminpritchard230 <104660463+benjaminpritchard230@users.noreply.github.com>
2023-03-25 23:31:24 +0000

    Add nextjs font from google fonts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,16 @@
 import { store } from "@/store/store";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { EB_Garamond } from "next/font/google";
 import { Provider } from "react-redux";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const ebGaramond = EB_Garamond({ subsets: ["latin"] });
   return (
     <Provider store={store}>
-      <Component {...pageProps} />
+      <main className={ebGaramond.className}>
+        <Component {...pageProps} />
+      </main>
     </Provider>
   );
 }
