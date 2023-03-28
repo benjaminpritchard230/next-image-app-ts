6c8e080dd733cfc548b5da9947b420e4b996e9ef
benjaminpritchard230 <104660463+benjaminpritchard230@users.noreply.github.com>
2023-03-28 22:10:14 +0100

    Remove unused favicon links

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,8 +1,10 @@
 // components/layout.js
 
 import Head from "next/head";
+import FloatingActionButtons from "./FloatingActionButtons";
 import Footer from "./Footer";
 import Navbar from "./Navbar";
+import NewPostDialog from "./NewPostDialog";
 
 export default function Layout({ children }: any) {
   return (
@@ -10,27 +12,11 @@ export default function Layout({ children }: any) {
       <Head>
         <title>Next Image App</title>
         <meta name="Layout" content="Next Movie Website" />
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon-16x16.png"
-        />
-        <link rel="manifest" href="/site.webmanifest" />
       </Head>
       <Navbar />
       <main>{children}</main>
+      <NewPostDialog />
+      <FloatingActionButtons />
       <Footer />
     </>
   );
