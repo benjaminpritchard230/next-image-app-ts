6e536531e5b9e2023f132f71d7acc763e8063851
benjaminpritchard230 <104660463+benjaminpritchard230@users.noreply.github.com>
2023-03-30 16:48:15 +0100

    Create component

---
+++ pages/my-profile/index.tsx
@@ -0,0 +1,10 @@
+import PrivateUserCard from "@/components/PrivateUserCard";
+import React from "react";
+
+type Props = {};
+
+const MyProfile = (props: Props) => {
+  return <PrivateUserCard />;
+};
+
+export default MyProfile;
