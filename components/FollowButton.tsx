b9ff28919d50f372775dbfa51237baf2b19d089e
benjaminpritchard230 <104660463+benjaminpritchard230@users.noreply.github.com>
2023-03-31 23:11:00 +0100

    Create FollowButton component

---
+++ components/FollowButton.tsx
@@ -0,0 +1,55 @@
+import {
+  useFollowUserMutation,
+  useGetCurrentUserInfoQuery,
+} from "@/features/api/apiSlice";
+import { Tooltip } from "@mui/material";
+import Button from "@mui/material/Button";
+
+type Props = {
+  id: string;
+};
+
+const FollowButton = ({ id }: Props) => {
+  const {
+    data: currentUserInfoData,
+    error,
+    isError,
+    isLoading,
+  } = useGetCurrentUserInfoQuery();
+
+  console.log(currentUserInfoData, "cuid");
+  const [followUser] = useFollowUserMutation();
+
+  const handleSubscribeClick = () => {
+    followUser(id);
+  };
+  if (currentUserInfoData != undefined) {
+    return (
+      <Tooltip
+        title={
+          !isLoading && currentUserInfoData.following.includes(parseInt(id))
+            ? "Click to unsubscribe"
+            : "Click to subscribe"
+        }
+        placement={"right"}
+      >
+        <Button
+          variant={
+            !isLoading && currentUserInfoData.following.includes(parseInt(id))
+              ? "outlined"
+              : "contained"
+          }
+          onClick={() => {
+            handleSubscribeClick();
+          }}
+        >
+          {!isLoading && currentUserInfoData.following.includes(parseInt(id))
+            ? "Unfollow"
+            : "Follow"}
+        </Button>
+      </Tooltip>
+    );
+  } else return <></>;
+};
+
+export default FollowButton;
