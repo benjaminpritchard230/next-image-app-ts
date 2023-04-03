c399b000419a7b84b2fd41351835f0801352bc5f
benjaminpritchard230 <104660463+benjaminpritchard230@users.noreply.github.com>
2023-04-04 00:41:00 +0100

    Configure store to use redux-persist

--- store/store.ts
+++ store/store.ts
@@ -2,18 +2,42 @@ import { postsApi } from "@/features/api/apiSlice";
 import authReducer from "@/features/auth/authSlice";
 import newPostDialogReducer from "@/features/dialog/newPostDialogSlice";
 import { configureStore } from "@reduxjs/toolkit";
+import {
+  FLUSH,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+  REHYDRATE,
+  persistReducer,
+  persistStore,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+const persistConfig = {
+  key: "root",
+  storage,
+  blacklist: [postsApi.reducerPath],
+};
+
+const persistedReducer = persistReducer(persistConfig, authReducer);
 
 export const store = configureStore({
   reducer: {
-    auth: authReducer,
+    auth: persistedReducer,
     newPostDialog: newPostDialogReducer,
     [postsApi.reducerPath]: postsApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(postsApi.middleware),
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }).concat(postsApi.middleware),
 });
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch;
+
+export const persistor = persistStore(store);
