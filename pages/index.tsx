d0de1990de80d6f79b03bb345c3f6cf577f4edf5
benjaminpritchard230 <104660463+benjaminpritchard230@users.noreply.github.com>
2023-04-09 00:19:44 +0100

    Add subscirbed/all posts toggle

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,11 @@
 import PostCard from "@/components/PostCard";
+import PublicPostsTopCard from "@/components/PublicPostsTopCard";
 import {
   useGetCurrentUserInfoQuery,
+  useGetFollowingPostsQuery,
   useGetPublicPostsQuery,
 } from "@/features/api/apiSlice";
+import { RootState } from "@/store/store";
 import { IPost, IPostsResponse } from "@/types/posts";
 import { CircularProgress } from "@mui/material";
 import Box from "@mui/material/Box";
@@ -10,19 +13,26 @@ import Paper from "@mui/material/Paper";
 import { styled } from "@mui/material/styles";
 import Grid from "@mui/material/Unstable_Grid2";
 import { GetStaticProps } from "next";
-import React from "react";
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
 type Props = {};
 
 const Homepage = ({}: Props) => {
+  const auth = useSelector((state: RootState) => state.auth);
+  const [subscriptionsOnly, setSubscriptionsOnly] = useState(false);
+
   const {
     data: publicPostsData,
     error,
     isError,
-    isLoading,
+    isLoading: isLoadingPublic,
   } = useGetPublicPostsQuery("1", { refetchOnMountOrArgChange: true });
 
-  const displayImagePosts = () => {
-    if (!isLoading && publicPostsData != undefined) {
+  const { data: subscribedPostsData, isLoading: isLoadingSubscribed } =
+    useGetFollowingPostsQuery();
+
+  const displayAllImagePosts = () => {
+    if (!isLoadingPublic && publicPostsData != undefined) {
       return publicPostsData!.results.map((post: IPost) => (
         <PostCard post={post} key={post.id} />
       ));
@@ -34,14 +44,38 @@ const Homepage = ({}: Props) => {
       );
     }
   };
+  const displaySubscribedImagePosts = () => {
+    if (!isLoadingSubscribed && subscribedPostsData != undefined) {
+      return subscribedPostsData!.map((post: IPost) => (
+        <PostCard post={post} key={post.id} />
+      ));
+    } else {
+      return (
+        <Box sx={{ display: "flex", justifyContent: "center" }}>
+          <CircularProgress />
+        </Box>
+      );
+    }
+  };
+
   const { data: currentUserInfoData } = useGetCurrentUserInfoQuery();
   console.log(currentUserInfoData);
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <Grid container spacing={0}>
-        {displayImagePosts()}
-      </Grid>
-    </Box>
+    <>
+      {auth.token.length > 0 ? (
+        <PublicPostsTopCard
+          subscriptionsOnly={subscriptionsOnly}
+          setSubscriptionsOnly={setSubscriptionsOnly}
+        />
+      ) : null}
+      <Box sx={{ flexGrow: 1 }}>
+        <Grid container spacing={0}>
+          {subscriptionsOnly
+            ? displaySubscribedImagePosts()
+            : displayAllImagePosts()}
+        </Grid>
+      </Box>
+    </>
   );
 };
 
